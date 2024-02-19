33973f4ba2b766fcbd4760928a13b4ca3466e087
derinzeyneb <159806600+derinzeyneb@users.noreply.github.com>
2024-02-19 17:29:17 +0300

    Update sozluk.py

--- sozluk.py
+++ sozluk.py
@@ -1,1 +1,10 @@
+meme_dict = {
+             "CREEPY" : "korkunc",
+             "LOL" : "Laugh out loud kisaltilmasi"
+            } 
 
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harfle yazın!) :
+if word in meme_dict.keys():
+             print(meme_dict[word]) 
+else :
+    print("uzgunum bu kelimenin anlamini bende bilmiyoru ")
